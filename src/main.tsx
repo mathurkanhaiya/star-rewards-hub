1872b8a6035408411865c748b61e5987bddd8202
mathurkanhaiya <156588144+mathurkanhaiya@users.noreply.github.com>
2026-03-20 13:00:58 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <TonConnectUIProvider manifestUrl="https://star-rewards-hub.vercel.app/tonconnect-manifest.json">
+    <App />
+  </TonConnectUIProvider>
+);
