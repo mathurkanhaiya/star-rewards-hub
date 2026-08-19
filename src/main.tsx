e0e0ddafd6ba167a97cd9663969521b37b89e474
mathurkanhaiya <156588144+mathurkanhaiya@users.noreply.github.com>
2026-08-19 17:12:32 +0530

    feat: load global dark light theme styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import "./theme.css";
 import { PreferencesProvider } from "./context/PreferencesContext";
 
 createRoot(document.getElementById("root")!).render(
